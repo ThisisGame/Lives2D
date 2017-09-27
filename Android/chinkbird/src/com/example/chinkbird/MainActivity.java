88daec885cd396acf66a9c3ed928734fe956bcb5
Captain <790621656@qq.com>
2017-09-28 01:45:43 +0800

    Android lives2dLibrary

---
+++ Android/chinkbird/src/com/example/chinkbird/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.chinkbird;
+
+
+import com.lives2d.library.lives2dActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends lives2dActivity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+	}
+}
