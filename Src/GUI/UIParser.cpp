76236784df83e13306ddd33d9352b314d912378f
captain <980097191@qq.com>
2018-02-09 19:01:57 +0800

    add uiparser

---
+++ Src/GUI/UIParser.cpp
@@ -0,0 +1,17 @@
+#include "UIParser.h"
+#include"PlayerPrefs/TinyXml/tinyxml.h"
+#include"Component/GameObject.h"
+
+
+UIParser::UIParser()
+{
+}
+
+
+UIParser::~UIParser()
+{
+}
+
+void UIParser::CreateUI(const char * varFilePath)
+{
+}
---
+++ Src/GUI/UIParser.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+
+
+class UIParser
+{
+public:
+	UIParser();
+	~UIParser();
+
+	void CreateUI(const char* varFilePath);
+};
+
