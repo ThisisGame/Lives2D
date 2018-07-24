ed9edecb56373faec887825d3ba930147d835622
陈鹏 <980097191@qq.com>
2018-07-24 22:55:25 +0800

    import osx glfw

---
+++ OSX/src/GLFW.build/Debug/glfw.build/Objects-normal/x86_64/cocoa_time.d
@@ -0,0 +1,11 @@
+dependencies: \
+  /Users/pengchen/Documents/GitHub/Lives2D_Depends/glfw/src/cocoa_time.c \
+  /Users/pengchen/Documents/GitHub/Lives2D_Depends/glfw/src/internal.h \
+  /Users/pengchen/Documents/GitHub/Lives2D_Depends/glfw/build/src/glfw_config.h \
+  /Users/pengchen/Documents/GitHub/Lives2D_Depends/glfw/src/../include/GLFW/glfw3.h \
+  /Users/pengchen/Documents/GitHub/Lives2D_Depends/glfw/src/cocoa_platform.h \
+  /Users/pengchen/Documents/GitHub/Lives2D_Depends/glfw/src/posix_thread.h \
+  /Users/pengchen/Documents/GitHub/Lives2D_Depends/glfw/src/cocoa_joystick.h \
+  /Users/pengchen/Documents/GitHub/Lives2D_Depends/glfw/src/nsgl_context.h \
+  /Users/pengchen/Documents/GitHub/Lives2D_Depends/glfw/src/egl_context.h \
+  /Users/pengchen/Documents/GitHub/Lives2D_Depends/glfw/src/osmesa_context.h
